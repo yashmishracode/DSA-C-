da5c10d0ebf16e738af392084540a1d18a6084b5
yashmishracode <yashmishracode>
2024-09-24 07:26:28 +0530

    start

---
+++ Missing_Elements_from_Array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void findMissing(int *a,int n ){
+    for(int i=0;i<n;i++){
+        int index = abs(a[i]);
+        if(a[index-1]>0){
+            a[index-1] *= -1;
+        }
+    }
+    for(int i = 0 ; i < n ; i++){
+        if(a[i]>0){
+            cout << i+1<< " ";
+        }
+    }
+
+}
+int main(){
+
+int a[] = {1,3,5,3,4};
+int n = sizeof(a)/sizeof(int);
+
+findMissing(a,n);
+}
--- test.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <iostream>
-
-int main() {
-   
-    std::cout << "Try programiz.pro";
-
-    return 0;
-}
